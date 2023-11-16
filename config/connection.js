1fa572f4f3c048c26a4b92b5c3bc959bba4b07f8
Hailey Strauss <hstrauss2001@gmail.com>
2023-11-16 15:06:32 -0500

    done

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,4 @@
 const Sequelize = require("sequelize");
-require("dotenv").config();
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
-
-const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
@@ -27,6 +9,7 @@ const sequelize = process.env.JAWSDB_URL
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD,
+
       {
         host: "localhost",
         dialect: "mysql",
