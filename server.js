719a06eacfc956dae792d5c51865b75c62d94693
Hailey Strauss <hstrauss2001@gmail.com>
2023-11-16 14:08:51 -0500

    update

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const expressHandlebars = require("express-handlebars");
+const path = require("path");
+require("body-parser");
+const helpers = require("./utils/helpers");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const exphbs = expressHandlebars.create({
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, "views/layouts"),
+  extname: ".handlebars",
+  helpers: helpers,
+});
+
+const hbs = expressHandlebars.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/index.js");
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
